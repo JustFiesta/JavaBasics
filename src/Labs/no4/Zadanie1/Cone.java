69078232d44dbfd83d2c14979d0e2878fe764050
JustFiesta <mateusz.bocak9@gmail.com>
2023-03-28 16:29:30 +0200

    .

--- src/Labs/no4/Zadanie1/Cone.java
+++ src/Labs/no4/Zadanie1/Cone.java
@@ -1,5 +1,36 @@
 package Labs.no4.Zadanie1;
 
 public class Cone {
+    String name;
+    double radius, l;
+    public double Field(double radius){
+        return 4 * Math.PI * (Math.pow(radius, 2));
+    }
+    public double Velocity(double radius){
+        return 4/3 * Math.PI * Math.pow(radius,3);
+    }
+    public void Params(){
+        System.out.println("Wybrana figura: "+this.getName());
+        System.out.println("Promień kuli wynosi: "+this.getRadius());
+        System.out.println("Pole wynosi: "+this.Field(radius));
+        System.out.println("Objętość wynosi: "+this.Velocity(radius));
+    }
 
+
+
+    public String getName() {
+        return name;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setL(double l) {
+        this.l = l;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- src/Labs/no4/Zadanie1/Main.java
+++ src/Labs/no4/Zadanie1/Main.java
@@ -1,94 +1,103 @@
 package Labs.no4.Zadanie1;
 
-import Lab.no4.Zadanie1.Square;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         //Menu
-        System.out.println("Jaką figurę chcesz policzyć?");
+        System.out.println("Jaką figurę chcesz policzyć? (wyjdź - 1)");
         String choice = InputString();
 
-        switch (choice){
-            case "koło" :
-                System.out.println("Podaj promień koła");
-                double radius = InputDouble();
+        int i = 0;
+        while(i == 0){
+            switch (choice) {
+                case "koło":
+                    System.out.println("Podaj promień koła");
+                    double radius = InputDouble();
 
-                Circle circle = new Circle();
-                circle.setRadius(radius);
-                circle.setName(choice);
-                circle.Params();
-                break;
+                    Circle circle = new Circle();
+                    circle.setRadius(radius);
+                    circle.setName(choice);
+                    circle.Params();
+                    break;
 
-            case "kwadrat":
-                System.out.println("Podaj podstawe kwadratu");
-                double base = InputDouble();
+                case "kwadrat":
+                    System.out.println("Podaj podstawe kwadratu");
+                    double base = InputDouble();
 
-                Square square = new Square();
-                square.setBase(base);
-                square.setName(choice);
-                square.Params();
-                break;
+                    Square square = new Square();
+                    square.setBase(base);
+                    square.setName(choice);
+                    square.Params();
+                    break;
 
-            case "prostokąt":
-                System.out.println("Podaj podstawę prostokąta");
-                base = InputDouble();
-                System.out.println("Podaj bok prostokąta");
-                double side = InputDouble();
+                case "prostokąt":
+                    System.out.println("Podaj podstawę prostokąta");
+                    base = InputDouble();
+                    System.out.println("Podaj bok prostokąta");
+                    double side = InputDouble();
 
-                Rectangle rectangle = new Rectangle();
-                rectangle.setBase(base);
-                rectangle.setSide(side);
-                rectangle.setName(choice);
-                rectangle.Params();
-                break;
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.setBase(base);
+                    rectangle.setSide(side);
+                    rectangle.setName(choice);
+                    rectangle.Params();
+                    break;
 
-            case "Sześcian":
-                System.out.println("Podaj podstawę sześcianu");
-                base = InputDouble();
+                case "Sześcian":
+                    System.out.println("Podaj podstawę sześcianu");
+                    base = InputDouble();
 
-                Cube cube = new Cube();
-                cube.setBase(base);
-                cube.setName(choice);
-                cube.Params();
-                break;
+                    Cube cube = new Cube();
+                    cube.setBase(base);
+                    cube.setName(choice);
+                    cube.Params();
+                    break;
 
-            case "Prostopadłościan":
-                System.out.println("Podaj podstawę prostopadłościanu");
-                base = InputDouble();
-                System.out.println("Podaj bok prostopadłościanu");
-                side = InputDouble();
-                System.out.println("Podaj wysokość prostopadłościanu");
-                double height = InputDouble();
+                case "prostopadłościan":
+                    System.out.println("Podaj podstawę prostopadłościanu");
+                    base = InputDouble();
+                    System.out.println("Podaj bok prostopadłościanu");
+                    side = InputDouble();
+                    System.out.println("Podaj wysokość prostopadłościanu");
+                    double height = InputDouble();
 
-                Cuboid cuboid = new Cuboid();
-                cuboid.setBase(base);
-                cuboid.setSide(side);
-                cuboid.setHeight(height);
-                cuboid.setName(choice);
-                cuboid.Params();
-                break;
-//
-//            case "Kula":
-//                System.out.println("Podaj promień koła");
-//                double radius = InputDouble();
-//                Circle circle = new Circle();
-//                circle.setRadius(radius);
-//                circle.setName(choice);
-//                circle.Params();
-//                break;
-//
-//            case "Stożek":
-//                System.out.println("Podaj promień koła");
-//                double radius = InputDouble();
-//                Circle circle = new Circle();
-//                circle.setRadius(radius);
-//                circle.setName(choice);
-//                circle.Params();
-//                break;
+                    Cuboid cuboid = new Cuboid();
+                    cuboid.setBase(base);
+                    cuboid.setSide(side);
+                    cuboid.setHeight(height);
+                    cuboid.setName(choice);
+                    cuboid.Params();
+                    break;
 
+                case "kula":
+                    System.out.println("Podaj promień Kuli");
+                    radius = InputDouble();
+
+                    Sphere sphere = new Sphere();
+                    sphere.setRadius(radius);
+                    sphere.setName(choice);
+                    sphere.Params();
+                    break;
+
+                case "stożek":
+                    System.out.println("Podaj promień podstawy stożka:");
+                    radius = InputDouble();
+                    System.out.println("Podaj tworzącą stożka:");
+                    double l = InputDouble();
+
+                    Cone cone = new Cone();
+                    cone.setRadius(radius);
+                    cone.setL(l);
+                    cone.setName(choice);
+                    cone.Params();
+                    break;
+                case "1":
+                    System.out.println("Adios!");
+                    i = 1;
+                    break;
+            }
         }
     }
     //Pobieranie info od uzytkownika
