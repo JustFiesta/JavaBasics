72b36e35f26820086009b83d10e6e79fa839b458
JustFiesta <mateusz.bocak9@gmail.com>
2023-04-08 00:34:36 +0200

    Zadanie 2 - lab 6 - wczytywanie floatów do poprawy

---
+++ src/Labs/no5/Zadanie2/Car.java
@@ -0,0 +1,83 @@
+package Labs.no5.Zadanie2;
+
+import static Labs.no5.Zadanie2.Main.Inputfloat;
+
+public class Car{
+    String make, model, chasis, color;
+    double mileage;
+    int productionYear;
+
+    public Car(String make, String model, String chasis, String color, int productionYear, double mileage) {
+        setMake(make);
+        setModel(model);
+        setChasis(chasis);
+        setColor(color);
+        setProductionYear(productionYear);
+        setMileage(mileage);
+    }
+    @Override
+    public String toString() {
+        return "Informacje o samochodzie: " +
+                "marka='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", nadwozie='" + chasis + '\'' +
+                ", kolor='" + color + '\'' +
+                ", rok produkcji=" + productionYear +
+                ", przebieg=" + mileage;
+    }
+
+    public void CheckMileage(){
+        if(this.mileage < 0) {
+            System.out.println("Ujemny przebieg! Proszę wprowadzić go ponownie");
+            this.mileage = (int) Inputfloat();
+        }else{}
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setChasis(String chasis) {
+        this.chasis = chasis;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getChasis() {
+        return chasis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getProductionYear() {
+        return productionYear;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+}
---
+++ src/Labs/no5/Zadanie2/Main.java
@@ -0,0 +1,29 @@
+package Labs.no5.Zadanie2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float liczba = Inputfloat();
+        PersonalCar fiat = new PersonalCar(4,1.8F,1.2F,"Fiat","Coupe", "coupe", "czerwony", 1998,273000);
+        fiat.CheckDisplacement(); fiat.CheckWeight(); fiat.CheckMileage();
+        System.out.println(fiat.toString());
+
+        Car uno = new Car("Fiat", "Uno", "hatchback", "srebrny", 2000,42000);
+        uno.CheckMileage();
+        System.out.println(uno.toString());
+
+        Car panda = new Car("Fiat", "Panda", "hatchback", "niebieski", 2005,147000);
+        panda.CheckMileage();
+        System.out.println(panda.toString());
+    }
+    public static float Inputfloat(){
+        Scanner UserInput = new Scanner(System.in);
+        float input = UserInput.nextFloat();
+        return input;
+    }
+    public static String InputString(){
+        Scanner UserText = new Scanner(System.in);
+        return UserText.next();
+    }
+}
