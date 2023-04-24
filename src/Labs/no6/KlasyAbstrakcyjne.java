ee11c1219a9dfd0a772d4229ebb63974a68c8b19
matthewbocake <mateusz.bocak9@gmail.com>
2023-04-24 08:17:53 +0200

    Szósty lab - klasy abstrakcji

---
+++ src/Labs/no6/KlasyAbstrakcyjne.java
@@ -0,0 +1,10 @@
+package Labs.no6;
+
+public abstract class KlasyAbstrakcyjne {
+    public static final int i = 10;
+    abstract int liczba();
+    public void witaj(){
+        System.out.println("Witaj!");
+    }
+
+}
