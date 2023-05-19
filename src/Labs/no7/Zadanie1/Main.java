7b2f0166753ee8e936323ef6f999576e91d8e7b1
JustFiesta <mateusz.bocak9@gmail.com>
2023-05-19 19:51:19 +0200

    .

--- src/Labs/no7/Zadanie1/Main.java
+++ src/Labs/no7/Zadanie1/Main.java
@@ -1,12 +1,10 @@
 package Labs.no7.Zadanie1;
-<<<<<<< HEAD
 //Połączenie z bazą danych
 //1. nowy projekt
 //2. pobranie sqlite-jbc lub inne
 //3. File>Project structure>Project Setting>Modules>DependenciesKlikamy + >Jar  or  Directory
 //4. kopia poniższego kodu do maina (na konsoli po odpaleniu powinno być „Opened database  successfully”)
-import java.sql.*;public class Main {public static void main( String args[] ) {Connection c = null;try {Class.forName("org.sqlite.JDBC");c = DriverManager.getConnection("jdbc:sqlite:test.db");} catch ( Exception e ) {System.err.println( e.getClass().getName() + ": " + e.getMessage() );System.exit(0);}System.out.println("Openeddatabase successfully");}}
-=======
+import java.sql.*;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -36,4 +34,3 @@ public class Main {
         System.out.print(String.format("Pirwiastek z %.4f = %.4f", userInput, Math.sqrt(userInput)));
     }
 }
->>>>>>> origin/master
