82e66c085a504d2f04b5fa86fe1d13394e4dcb25
matthewbocake <mateusz.bocak9@gmail.com>
2023-10-04 14:46:49 +0200

    Lab1 ASD - algorytmy usprawnienie x6.2

--- src/ASD/Podstawowe_algorytmy/Trójki_pitagorejskie.java
+++ src/ASD/Podstawowe_algorytmy/Trójki_pitagorejskie.java
@@ -6,8 +6,8 @@ public class Trójki_pitagorejskie {
         //Znajdywanie trójek pitagorejskich
 
         for (int a = 1; a <= 20; a++){
-            for (int b = 1; b <= 20; b++){
-                for (int c = 1; c <= 20; c++){
+            for (int b = a + 1; b <= 20; b++){
+                for (int c = b + 1; c <= 20; c++){
                     if (a * a + b * b == c * c){
                         System.out.println("Znalezione trójki: " + a + " " + b + " " + c);
 
