f7f62d4814a17c1ebd8a037ec3a567e58f91085a
JustFiesta <mateusz.bocak9@gmail.com>
2023-10-25 20:38:24 +0200

    Newton binominal dynamic

---
+++ src/ASD/Basic_algorythms/DynamicProgrammig/README.md
@@ -0,0 +1,6 @@
+### How do I understand dynamic approach?
+Well, it is a divide and conquer WITH remembering of states. But it requires elementary cases already in array. So baisicly you can take math formula like some function with intervals and make elementary cases.  
+
+Then instead of **recursion** you **iterate** and fetch previous elements from array - so it does not calculate same things again. 
+
+Like so in fibbonaci.
--- src/ASD/Basic_algorythms/DynamicProgrammig/binominalDynamic.java
+++ src/ASD/Basic_algorythms/DynamicProgrammig/binominalDynamic.java
@@ -6,5 +6,26 @@ public class binominalDynamic {
     //wzor do zaimplementowania: (n nad k) = 1                          dla k=0 lub k=n,
     //                                      (n-1 nad k-1) + (n-1 nad k) dla 0<k<n
 
+    static int[][] arrayForStates = new int[6][6];
 
+    public static void main(String[] args) {
+        newton(5, 3);
+    }
+    public static void newton(int n, int k){
+        for (int i = 1; i < arrayForStates.length; i++){
+            for (int j = 1; j < arrayForStates.length; j++){
+                if (i == j){
+                    arrayForStates[i][j] = 1;
+                } else if (i < j) {
+                    //regarding to our formula we have to skip those where k > n (it has to be 0 < k < n)
+                    System.out.print("s" + "\t");
+                    continue;
+                } else {
+                    arrayForStates[i][j] = arrayForStates[i - 1][j - 1] + arrayForStates[i - 1][j];
+                }
+                System.out.print(arrayForStates[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
