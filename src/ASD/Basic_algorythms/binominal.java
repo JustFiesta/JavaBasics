030dd89571259182524c432f9be61d3fd072b0fc
matthewbocake <mateusz.bocak9@gmail.com>
2023-10-25 13:51:17 +0200

    binominal algorithm - divide and conquer

---
+++ src/ASD/Basic_algorythms/binominal.java
@@ -0,0 +1,15 @@
+package ASD.Basic_algorythms;
+
+public class binominal {
+
+    public static void main(String[] args) {
+        System.out.println("dla 5 i 3");
+        System.out.println(Newton(5, 3));
+    }
+    public static int Newton(int n, int k){
+        if (k == 0 || k == n){
+            return 1;
+        }
+        return Newton(n-1, k-1) + Newton(n-1, k);
+    }
+}
