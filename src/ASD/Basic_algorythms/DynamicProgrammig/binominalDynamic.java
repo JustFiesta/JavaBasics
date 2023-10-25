f8454b21483ccb82302791655c6c8c9b4f0542ba
matthewbocake <mateusz.bocak9@gmail.com>
2023-10-25 14:43:36 +0200

    excercise sketch

--- src/ASD/Basic_algorythms/DynamicProgrammig/binominalDynamic.java
+++ src/ASD/Basic_algorythms/DynamicProgrammig/binominalDynamic.java
@@ -3,4 +3,6 @@ package ASD.Basic_algorythms.DynamicProgrammig;
 public class binominalDynamic {
     //tablica dwuwymiarowa na stany
     //rekurencja -> zmiana na pętle
+    //wzor do zaimplementowania: (n nad k) = 1                          dla k=0 lub k=n,
+    //                                      (n-1 nad k-1) + (n-1 nad k) dla 0<k<n
 }
